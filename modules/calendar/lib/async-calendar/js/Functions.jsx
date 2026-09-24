2216e148b12ffb2ce3a2db41e185a03159dddbd4
Richa Ferry Setyawan <aakricha2@gmail.com>
2026-09-24 16:53:45 +0700

    Keep styled calendar form buttons

--- modules/calendar/lib/async-calendar/js/Functions.jsx
+++ modules/calendar/lib/async-calendar/js/Functions.jsx
@@ -130,7 +130,7 @@ export function getPostLinksElement(linkData, handleOnClick) {
     if (linkData.url) {
         return (<a key={`link-${linkData.url}-${linkData.label}`} href={linkData.url} className={className}>{linkData.label}</a>);
     } else if (linkData.action) {
-        className = (className + ' publishpress-calendar-popup-action-button').trim();
+        className = className || 'publishpress-calendar-popup-action-button';
         return (<button type="button" key={`link-${linkData.url}-${linkData.label}`} className={className} onClick={(e) => handleOnClick(e, linkData)}>{linkData.label}</button>);
     }
 }
