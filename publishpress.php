b87ab6b30874bb3c99b65b9d80d83eced3b763f4
Richa Ferry Setyawan <aakricha2@gmail.com>
2026-09-26 02:44:59 +0700

    bump version & update changelog

--- publishpress.php
+++ publishpress.php
@@ -3,7 +3,7 @@
  * Plugin Name: PublishPress Planner Free
  * Plugin URI: https://publishpress.com/
  * Description: PublishPress Planner helps you plan and publish content inside WordPress. Features include a content calendar, kanban board, and notifications.
- * Version: 4.8.0
+ * Version: 4.8.1
  * Author: PublishPress
  * Author URI: https://publishpress.com
  * Text Domain: publishpress
